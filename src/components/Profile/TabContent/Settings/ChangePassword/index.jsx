1470da626ed8902c89f62ce4fcd0c2b922b682b9
somukhan <mhsomrat154@gmail.com>
2024-01-25 22:30:23 +0600

    implement form validation and styling issue at change password form at settings tab

--- src/components/Profile/TabContent/Settings/ChangePassword/index.jsx
+++ src/components/Profile/TabContent/Settings/ChangePassword/index.jsx
@@ -1,16 +1,46 @@
 import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { ChangePasswordSchema } from './utils'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 export default function ChangePassword() {
   const [currentPasswordType, setCurrentPasswordType] = useState('password')
   const [newPasswordType, setNewPasswordType] = useState('password')
   const [confirmPasswordType, setConfirmPasswordType] = useState('password')
 
+  const [changePasswordState, setChangePasswordState] = useState({
+    currentPassword: '',
+    newPassword: '',
+    confirmNewPassword: '',
+  })
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+  } = useForm({ resolver: zodResolver(ChangePasswordSchema) })
+
+  const handleOnChange = (event) => {
+    setChangePasswordState((prev) => ({
+      ...prev,
+      [event.target.id]: event.target.value,
+    }))
+  }
+
+  const onSubmit = (formData) => {
+    console.log(formData)
+  }
+
   return (
     <div className="w-full rounded-md border border-gray-200">
       <h1 className="border-b border-gray-200 p-4 text-xl font-medium text-[#1A1A1A]">
         Change Password
       </h1>
-      <form className="flex flex-col gap-4 p-4">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col gap-4 p-4"
+      >
         <div className="flex items-center justify-between gap-4">
           <div className="mb-12 flex w-full flex-col gap-2">
             <label htmlFor="currentPassword" className="text-sm text-[#1A1A1A]">
@@ -21,8 +51,8 @@ export default function ChangePassword() {
                 type={currentPasswordType === 'password' ? 'password' : 'text'}
                 id="currentPassword"
                 name="currentPassword"
-                // {...register('currentPassword')}
-                // value={profile.currentPassword}
+                {...register('currentPassword')}
+                // value={changePasswordState.currentPassword}
                 // onChange={handleOnChange}
                 placeholder="Enter Current Password"
                 className="absolute left-0 right-0 top-0 rounded-md border border-gray-200 p-2 text-base text-[#666666] outline-none ring-offset-indigo-500 transition-all duration-200 ease-in-out focus:ring-2"
@@ -43,15 +73,15 @@ export default function ChangePassword() {
                 )}
               </button>
             </div>
-            {/* {errors['currentPassword'] && (
-              <p className="text-xs text-red-700">
+            {errors['currentPassword'] && (
+              <p className="-mb-10 mt-10 text-xs text-red-700">
                 {errors['currentPassword'].message}
               </p>
-            )} */}
+            )}
           </div>
         </div>
 
-        <div className="mb-6 flex flex-wrap items-center justify-between gap-16 md:gap-4">
+        <div className="mb-6 flex flex-wrap items-start justify-between gap-16 md:flex-nowrap md:gap-4">
           <div className="flex w-full flex-col gap-2">
             <label htmlFor="newPassword" className="text-sm text-[#1A1A1A]">
               New Password
@@ -61,8 +91,8 @@ export default function ChangePassword() {
                 type={newPasswordType === 'password' ? 'password' : 'text'}
                 id="newPassword"
                 name="newPassword"
-                // {...register('newPassword')}
-                // value={profile.newPassword}
+                {...register('newPassword')}
+                // value={changePasswordState.newPassword}
                 // onChange={handleOnChange}
                 placeholder="Enter New Password"
                 className="absolute left-0 right-0 top-0 rounded-md border border-gray-200 p-2 text-base text-[#666666] outline-none ring-offset-indigo-500 transition-all duration-200 ease-in-out focus:ring-2"
@@ -84,23 +114,26 @@ export default function ChangePassword() {
                 )}
               </button>
             </div>
-            {/* {errors['newPassword'] && (
-              <p className="text-xs text-red-700">
+            {errors['newPassword'] && (
+              <p className="-mb-10 mt-10 text-xs text-red-700">
                 {errors['newPassword'].message}
               </p>
-            )} */}
+            )}
           </div>
           <div className="flex w-full flex-col gap-2">
-            <label htmlFor="confirmPassword" className="text-sm text-[#1A1A1A]">
+            <label
+              htmlFor="confirmNewPassword"
+              className="text-sm text-[#1A1A1A]"
+            >
               Confirm Password
             </label>
             <div className="relative w-full">
               <input
                 type={confirmPasswordType === 'password' ? 'password' : 'text'}
-                id="confirmPassword"
-                name="confirmPassword"
-                // {...register('confirmPassword')}
-                // value={profile.confirmPassword}
+                id="confirmNewPassword"
+                name="confirmNewPassword"
+                {...register('confirmNewPassword')}
+                // value={changePasswordState.confirmPassword}
                 // onChange={handleOnChange}
                 placeholder="Confirm New Password"
                 className="absolute left-0 right-0 top-0 rounded-md border border-gray-200 p-2 text-base text-[#666666] outline-none ring-offset-indigo-500 transition-all duration-200 ease-in-out focus:ring-2"
@@ -121,11 +154,11 @@ export default function ChangePassword() {
                 )}
               </button>
             </div>
-            {/* {errors['confirmPassword'] && (
-              <p className="text-xs text-red-700">
-                {errors['confirmPassword'].message}
+            {errors['confirmNewPassword'] && (
+              <p className="-mb-10 mt-10 text-xs text-red-700">
+                {errors['confirmNewPassword'].message}
               </p>
-            )} */}
+            )}
           </div>
         </div>
 
---
+++ src/components/Profile/TabContent/Settings/ChangePassword/utils.js
@@ -0,0 +1,14 @@
+import { z } from 'zod'
+
+export const ChangePasswordSchema = z
+  .object({
+    currentPassword: z.string().min(1, 'Please provide current password'),
+    newPassword: z
+      .string()
+      .min(6, 'Password should be at least of 6 characters'),
+    confirmNewPassword: z.string().min(1, 'Please confirm new password'),
+  })
+  .refine((data) => data.newPassword !== data.confirmNewPassword, {
+    message: 'Passwords did not match',
+    path: ['confirmNewPassword'],
+  })
