61bf9733a6c3c6f946903786ddc624ebc85f8a33
somukhan <mhsomrat154@gmail.com>
2024-01-26 00:18:09 +0600

    make it more user friendly

--- src/components/PrivateRoute/index.jsx
+++ src/components/PrivateRoute/index.jsx
@@ -3,6 +3,14 @@ import { useEffect, useState } from 'react'
 import { useAuthStore } from '@/store/auth'
 import { useRouter } from 'next/navigation'
 
+const VerifyingIdentity = () => (
+  <div className="fixed bottom-0 left-0 right-0 top-0 z-[1000] flex items-center justify-center bg-black">
+    <h2 className="text-2xl font-bold tracking-wide text-white md:text-4xl">
+      Verifying Identity...
+    </h2>
+  </div>
+)
+
 export default function PrivateRoute({ children }) {
   const router = useRouter()
   const [isClient, setIsClient] = useState(false)
@@ -18,18 +26,12 @@ export default function PrivateRoute({ children }) {
 
       if (!isLoggedIn) {
         router.replace('/')
-        return <></>
+        return <VerifyingIdentity />
       }
 
       return <>{Array.isArray(children) ? children : [children]}</>
     }
   }
 
-  return (
-    <div className="fixed bottom-0 left-0 right-0 top-0 z-[1000] flex items-center justify-center bg-black opacity-95">
-      <h2 className="text-2xl font-bold tracking-wide text-white md:text-4xl">
-        Verifying Identity...
-      </h2>
-    </div>
-  )
+  return <VerifyingIdentity />
 }
