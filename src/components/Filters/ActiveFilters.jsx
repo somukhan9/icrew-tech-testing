786536a3dceb8ff9836b8056d45a4c9ee2323a57
somukhan <mhsomrat154@gmail.com>
2024-01-30 18:29:56 +0600

    add sortBy, sortOrder and show(limit) to the active filter component

--- src/components/Filters/ActiveFilters.jsx
+++ src/components/Filters/ActiveFilters.jsx
@@ -7,7 +7,9 @@ export default function ActiveFilters() {
   const category = searchParams.get('category') || ''
   const price = searchParams.get('price') || ''
   const rating = searchParams.get('rating') || ''
-  const sorting = searchParams.get('sorting') || 'Latest'
+  const sortBy = searchParams.get('sortBy') || ''
+  const sortOrder = searchParams.get('sortOrder') || ''
+  const limit = searchParams.get('limit') || ''
 
   const router = useRouter()
 
@@ -52,6 +54,51 @@ export default function ActiveFilters() {
               onClick={() => {
                 const searchParams = new URLSearchParams(window.location.search)
                 searchParams.delete('rating')
+                router.replace(`?${searchParams.toString()}`, { scroll: false })
+              }}
+              className="bx bx-x bx-xs ml-3 text-gray-400 group-hover:text-black"
+            ></i>
+          </div>
+        ) : (
+          ''
+        )}
+        {sortBy ? (
+          <div className="group mr-3 flex w-max cursor-pointer items-center justify-between font-medium">
+            <p>{sortBy}</p>
+            <i
+              onClick={() => {
+                const searchParams = new URLSearchParams(window.location.search)
+                searchParams.delete('sortBy')
+                router.replace(`?${searchParams.toString()}`, { scroll: false })
+              }}
+              className="bx bx-x bx-xs ml-3 text-gray-400 group-hover:text-black"
+            ></i>
+          </div>
+        ) : (
+          ''
+        )}
+        {sortOrder ? (
+          <div className="group mr-3 flex w-max cursor-pointer items-center justify-between font-medium">
+            <p>{sortOrder}</p>
+            <i
+              onClick={() => {
+                const searchParams = new URLSearchParams(window.location.search)
+                searchParams.delete('sortOrder')
+                router.replace(`?${searchParams.toString()}`, { scroll: false })
+              }}
+              className="bx bx-x bx-xs ml-3 text-gray-400 group-hover:text-black"
+            ></i>
+          </div>
+        ) : (
+          ''
+        )}
+        {limit ? (
+          <div className="group mr-3 flex w-max cursor-pointer items-center justify-between font-medium">
+            <p>{limit}</p>
+            <i
+              onClick={() => {
+                const searchParams = new URLSearchParams(window.location.search)
+                searchParams.delete('limit')
                 router.replace(`?${searchParams.toString()}`, { scroll: false })
               }}
               className="bx bx-x bx-xs ml-3 text-gray-400 group-hover:text-black"
